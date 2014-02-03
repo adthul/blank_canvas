c0a6075f2fc7606a94dbdf63dc4e37620f2fe732
Andrew Thul <thul.andy@gmail.com>
2014-02-03 13:29:11 -0800

    add devise and generate devise User model

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ BlankCanvas::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'www.blank_canvas.com' }
 end
