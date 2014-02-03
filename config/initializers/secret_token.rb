2bf067055e5c5dd35377cbdd8cd4812884b27c98
Andrew Thul <thul.andy@gmail.com>
2014-02-03 11:43:07 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlankCanvas::Application.config.secret_key_base = 'fb692a9b211d361c5e5c143328efa301fc6d0b01a0a9caf8e6dbf641832cd236862d09a007fc21f5398453f0a357edbd76630c4088116045b6e0f4007dae7b17'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BlankCanvas::Application.config.session_store :cookie_store, key: '_blank_canvas_session'
