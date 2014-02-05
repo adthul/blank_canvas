47ad1a22d7c6d31b8c9e42f46dba5732f7f71050
Andrew Thul <thul.andy@gmail.com>
2014-02-05 15:21:47 -0800

    update user show html and css

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
   def set_avatar(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=512"
     image_tag(gravatar_url, alt: user.user_name, class: "gravatar")
   end
 end
