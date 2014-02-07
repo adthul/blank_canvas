36a25aacf7feb5c03ed9395cccb452d7568a4243
Andrew Thul <thul.andy@gmail.com>
2014-02-07 10:39:13 -0800

    update paths for link creation. fix link index

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -18,6 +18,7 @@ class FoldersController < ApplicationController
 
   def new
     @folder = @user.folders.build
+    @folder.folder_name = ""
   end
 
   def edit
--- app/models/folder.rb
+++ app/models/folder.rb
@@ -3,7 +3,6 @@ class Folder < ActiveRecord::Base
   belongs_to :link
 
   def self.destroy_folder(u_id, f_name)
-    binding.pry
     Folder.destroy_all(user_id: u_id, folder_name: f_name)
   end
 end
