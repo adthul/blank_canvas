046a75544b7e7e33d4566ae01a977e83a85d0a1d
Andre Smith <andrepsmithjr@gmail.com>
2014-02-07 08:55:15 -0800

    wip

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,4 +1,10 @@
 class Folder < ActiveRecord::Base
   belongs_to :user
   belongs_to :link
+
+  # def unique?(user,link_id)
+  #   matched = Folder.where(user_id: user.id, link_id: link_id)
+
+  #   matched.blank? ? return true : return false
+  # end
 end
